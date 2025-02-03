0f10ac81132db26137a62e9b4651e7fce9b9116e
SoaMariaka19 <hei.soa.2@gmail.com>
2025-02-03 13:22:36 +0300

    style: change font to fix uppercase to lowercase issue

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,10 +7,10 @@ const Home = () => {
     <HomeContainer id="home">
       <StyledHomeBox>
         <Box>
-          <StyledTitle variant="h4" gutterBottom>
+          <StyledTitle variant="h2" gutterBottom>
             Hello! I'm Soa Mariaka
           </StyledTitle>
-          <StyledSubtitle variant="h6" paragraph>
+          <StyledSubtitle variant="h5" paragraph>
             Junior Front-End Developer, passionate about creating intuitive and efficient interfaces. Curious and motivated, I'm constantly learning and evolving to improve. My goal? Build a fulfilling career in web development          
           </StyledSubtitle>
         </Box>
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -10,7 +10,7 @@ const theme = createTheme({
     },
   },
   typography: {
-    fontFamily: 'Kiona, sans-serif',
+    fontFamily: 'Cardo, serif',
   }
 });
 
