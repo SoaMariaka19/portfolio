4f4e41ce04b70d91c52ebbdc2f319a3dbbe06cd8
SoaMariaka19 <hei.soa.2@gmail.com>
2025-02-04 20:10:55 +0300

    feat: add email validation and disable submit button if fields are incomplete

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,19 +1,43 @@
-import React, { useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import { Typography, Grid, IconButton, Box, Snackbar, Alert } from '@mui/material';
 import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 import { StyledForm, StyledFormButton, StyledContactContainer, StyledTextField, StyledTypography } from '../styles/customStyles';
 import emailjs from '@emailjs/browser';
 
 const Contact = () => {
-  const form = useRef<HTMLFormElement>(null); 
+  const form = useRef<HTMLFormElement>(null);
+  const [formData, setFormData] = useState({
+    user_name: '',
+    user_email: '',
+    message: ''
+  });
+  const [emailError, setEmailError] = useState('');
   const [alertMessage, setAlertMessage] = useState('');
   const [alertType, setAlertType] = useState<'success' | 'error'>('success');
   const [openAlert, setOpenAlert] = useState(false);
 
+  const validateEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setEmailError("Format d'email invalide");
+    } else {
+      setEmailError('');
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+
+    if (name === 'user_email') {
+      validateEmail(value);
+    }
+  };
+
   const sendEmail = (e: React.FormEvent) => {
     e.preventDefault();
 
-    if (form.current) { 
+    if (form.current) {
       emailjs
         .sendForm('service_5zmjd59', 'template_dhx5qzk', form.current, {
           publicKey: 'nDkqitaE9l0p9K29Z',
@@ -23,7 +47,9 @@ const Contact = () => {
             setAlertMessage('Message envoyé avec succès!');
             setAlertType('success');
             setOpenAlert(true);
-            form.current?.reset(); 
+            setFormData({ user_name: '', user_email: '', message: '' });
+            setEmailError('');
+            form.current?.reset();
           },
           (error) => {
             setAlertMessage(`Échec de l'envoi: ${error.text}`);
@@ -37,6 +63,8 @@ const Contact = () => {
   const handleCloseAlert = () => {
     setOpenAlert(false);
   };
+
+  const isFormValid = formData.user_name && formData.user_email && formData.message && !emailError;
 
   return (
     <StyledContactContainer id='contact'>
@@ -63,18 +91,66 @@ const Contact = () => {
 
         <Grid item xs={12} sm={6}>
           <StyledForm ref={form} onSubmit={sendEmail}>
-            <StyledTextField label="Nom" variant="outlined" fullWidth margin="normal" name="user_name" />
-            <StyledTextField label="Email" variant="outlined" fullWidth margin="normal" name="user_email" />
-            <StyledTextField label="Message" variant="outlined" fullWidth multiline rows={4} margin="normal" name="message" />
-            <StyledFormButton type="submit" variant="contained" color="primary">
+            <StyledTextField 
+              label="Nom" 
+              variant="outlined" 
+              fullWidth 
+              margin="normal" 
+              name="user_name"
+              value={formData.user_name}
+              onChange={handleChange}
+              InputProps={{ style: { fontSize: '1.2rem' } }} 
+              InputLabelProps={{ style: { fontSize: '1rem' } }}
+            />
+            <StyledTextField 
+              label="Email" 
+              variant="outlined" 
+              fullWidth 
+              margin="normal" 
+              name="user_email"
+              value={formData.user_email}
+              onChange={handleChange}
+              error={!!emailError}
+              helperText={emailError}
+              FormHelperTextProps={{ sx: { fontSize: '1.2rem' } }}
+              InputProps={{ style: { fontSize: '1.2rem' } }}
+              InputLabelProps={{ style: { fontSize: '1rem' } }} 
+            />
+            <StyledTextField 
+              label="Message" 
+              variant="outlined" 
+              fullWidth 
+              multiline 
+              rows={4} 
+              margin="normal" 
+              name="message"
+              value={formData.message}
+              onChange={handleChange}            
+              InputProps={{ style: { fontSize: '1.2rem' } }} 
+              InputLabelProps={{ style: { fontSize: '1rem' } }} 
+            />
+            <StyledFormButton type="submit" variant="contained" color="primary" disabled={!isFormValid}>
               Envoyer
             </StyledFormButton>
           </StyledForm>
         </Grid>
       </Grid>
 
-      <Snackbar open={openAlert} autoHideDuration={6000} onClose={handleCloseAlert}>
-        <Alert onClose={handleCloseAlert} severity={alertType} sx={{ width: '100%' }}>
+      <Snackbar 
+        open={openAlert} 
+        autoHideDuration={6000} 
+        onClose={handleCloseAlert} 
+        sx={{ width: '30%' }}
+      >
+        <Alert 
+          onClose={handleCloseAlert} 
+          severity={alertType} 
+          sx={{ 
+            width: '50%', 
+            fontSize: '1.2rem', 
+            padding: '10px 20px', 
+          }}
+        >
           {alertMessage}
         </Alert>
       </Snackbar>
